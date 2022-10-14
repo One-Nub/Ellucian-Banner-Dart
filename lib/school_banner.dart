01a745bc944fdd20a9c62d9a2aa6fb8585b64cb7
Nub <38899321+One-Nub@users.noreply.github.com>
2022-10-14 14:26:55 -0500

    Export Subject & Term classes

--- lib/school_banner.dart
+++ lib/school_banner.dart
@@ -3,3 +3,5 @@ export 'src/school_banner_base.dart';
 
 export 'src/structures/course.dart';
 export 'src/structures/meeting_time.dart';
+export 'src/structures/subject.dart';
+export 'src/structures/term.dart';
